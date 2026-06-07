3f22f0bb392c75abfb10af96731d880aa73f53e7
Noorkey <nooabs080109@student.jenseneducation.se>
2026-06-07 19:22:25 +0200

    Jag har skapat en responsiv webbplats med Bootstrap
    Projektet är byggt med HTML och Bootstrap för att skapa en snygg och mobilanpassad webbplats. Jag har använt Bootstrap-komponenter som navbar, grid-system, cards och collapse för att strukturera innehållet och göra sidan interaktiv och responsiv.

--- init.js
+++ init.js
@@ -1,25 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // HEADER
+  // HEADER (Bootstrap navbar + dropdown)
   document.body.insertAdjacentHTML("afterbegin", `
-    <header class="bg-dark text-white p-3">
-      <div class="container d-flex justify-content-between align-items-center">
-        <h3 class="m-0">My Favorites</h3>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
 
-        <nav>
-          <a class="text-white mx-2 text-decoration-none" href="index.html">Home</a>
-          <a class="text-white mx-2 text-decoration-none" href="movies.html">Movies</a>
-          <a class="text-white mx-2 text-decoration-none" href="series.html">Series</a>
-          <a class="text-white mx-2 text-decoration-none" href="music.html">Music</a>
-        </nav>
+        <a class="navbar-brand fw-bold" href="index.html">MediaHub</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav">
+
+          <ul class="navbar-nav ms-auto">
+
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                Movies
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="movies.html">All Movies</a></li>
+                <li><a class="dropdown-item" href="#">2000s</a></li>
+                <li><a class="dropdown-item" href="#">1900s</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="series.html">Series</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="music.html">Music</a>
+            </li>
+
+          </ul>
+
+        </div>
       </div>
-    </header>
+    </nav>
   `);
 
   // FOOTER
   document.body.insertAdjacentHTML("beforeend", `
     <footer class="bg-dark text-white text-center p-3 mt-5">
-      <p class="m-0">© 2026 My Favorites</p>
+      <p class="m-0">© 2026 MediaHub</p>
     </footer>
   `);
 
